1c8c6f7fc71f9e011b43c911c18b703ddc05ad72
manhtan <manhtan.doan@stud.h-da.de>
2023-01-19 00:28:08 +0100

    recyclerview in doctor notification and request examination

---
+++ app/src/main/java/com/example/krankenhaus/ui/doctor/ui/main/ExaminationAdapter.java
@@ -0,0 +1,183 @@
+package com.example.krankenhaus.ui.doctor.ui.main;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.krankenhaus.R;
+import com.example.krankenhaus.srccode.entities.relations.BloodTestAndRecord;
+import com.example.krankenhaus.srccode.entities.relations.MRIAndRecord;
+import com.example.krankenhaus.srccode.entities.relations.RecordAndPatient;
+import com.example.krankenhaus.ui.administrator.ui.main.AdminBedAdapter;
+
+import org.w3c.dom.Text;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExaminationAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<Object> examinationList;
+    private List<MRIAndRecord> mriAndRecordList;
+    private List<BloodTestAndRecord> bloodTestAndRecordList;
+
+    DoctorViewModel doctorViewModel;
+    RecordAndPatient recordAndPatient;
+    LifecycleOwner lifecycleOwner;
+
+    private OnItemClickListener listener;
+
+    public ExaminationAdapter(DoctorViewModel doctorViewModel, LifecycleOwner lifecycleOwner) {
+        this.examinationList = new ArrayList<>();
+        this.doctorViewModel = doctorViewModel;
+        this.lifecycleOwner = lifecycleOwner;
+
+        doctorViewModel.getAllMriAndRecords().observe(lifecycleOwner, new Observer<List<MRIAndRecord>>() {
+            @Override
+            public void onChanged(List<MRIAndRecord> mriAndRecords) {
+                examinationList.addAll(mriAndRecords);
+            }
+        });
+
+        doctorViewModel.getAllBloodTestAndRecords().observe(lifecycleOwner, new Observer<List<BloodTestAndRecord>>() {
+            @Override
+            public void onChanged(List<BloodTestAndRecord> bloodTestAndRecords) {
+                examinationList.addAll(bloodTestAndRecords);
+            }
+        });
+    }
+
+    class MriAndRecordHolder extends RecyclerView.ViewHolder {
+        private TextView textViewName;
+        private TextView textViewDate;
+        private TextView textViewPatientName;
+        private TextView textViewBedNumber;
+
+        public MriAndRecordHolder(View itemView) {
+            super(itemView);
+            textViewName = itemView.findViewById(R.id.doctor_exam_name);
+            textViewDate = itemView.findViewById(R.id.doctor_exam_date);
+            textViewPatientName = itemView.findViewById(R.id.doctor_exam_patient_name);
+            textViewBedNumber = itemView.findViewById(R.id.doctor_exam_bed_number);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if (listener != null && position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(examinationList.get(position));
+                    }
+                }
+            });
+        }
+    }
+
+    class BloodTestAndRecordHolder extends RecyclerView.ViewHolder {
+        private TextView textViewName;
+        private TextView textViewDate;
+        private TextView textViewPatientName;
+        private TextView textViewBedNumber;
+
+        public BloodTestAndRecordHolder(View itemView) {
+            super(itemView);
+            textViewName = itemView.findViewById(R.id.doctor_exam_name);
+            textViewDate = itemView.findViewById(R.id.doctor_exam_date);
+            textViewPatientName = itemView.findViewById(R.id.doctor_exam_patient_name);
+            textViewBedNumber = itemView.findViewById(R.id.doctor_exam_bed_number);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if (listener != null && position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(examinationList.get(position));
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (examinationList.get(position) instanceof MRIAndRecord) {
+            return 0;
+        }
+        else if (examinationList.get(position) instanceof BloodTestAndRecord) {
+            return 1;
+        }
+        return -1;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.doctor_examination_item, parent, false);
+
+        switch (viewType) {
+            case 0: return new MriAndRecordHolder(itemView);
+            case 2: return new BloodTestAndRecordHolder(itemView);
+            default: break;
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        switch (getItemViewType(position)) {
+            case 0:
+                MriAndRecordHolder mriAndRecordHolder = (MriAndRecordHolder) holder;
+                MRIAndRecord mriAndRecord = (MRIAndRecord) examinationList.get(position);
+
+                doctorViewModel.getRecordAndPatientByRecordID(mriAndRecord.record.getRecordId()).observe(lifecycleOwner, new Observer<RecordAndPatient>() {
+                    @Override
+                    public void onChanged(RecordAndPatient input) {
+                        recordAndPatient = input;
+                        mriAndRecordHolder.textViewName.setText("MRI");
+                        mriAndRecordHolder.textViewDate.setText(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(mriAndRecord.mri.getExecutionTimestamp()));
+                        mriAndRecordHolder.textViewPatientName.setText(recordAndPatient.patient.getName());
+                        mriAndRecordHolder.textViewBedNumber.setText(Integer.toString(recordAndPatient.patient.getBedNumber()));
+                    }
+                });
+                break;
+
+            case 2:
+                BloodTestAndRecordHolder bloodTestAndRecordHolder = (BloodTestAndRecordHolder) holder;
+                BloodTestAndRecord bloodTestAndRecord = (BloodTestAndRecord) examinationList.get(position);
+
+                doctorViewModel.getRecordAndPatientByRecordID(bloodTestAndRecord.record.getRecordId()).observe(lifecycleOwner, new Observer<RecordAndPatient>() {
+                    @Override
+                    public void onChanged(RecordAndPatient input) {
+                        recordAndPatient = input;
+                        bloodTestAndRecordHolder.textViewName.setText("Blood Test");
+                        bloodTestAndRecordHolder.textViewDate.setText(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(bloodTestAndRecord.bloodTest.getExecutionTimestamp()));
+                        bloodTestAndRecordHolder.textViewPatientName.setText(recordAndPatient.patient.getName());
+                        bloodTestAndRecordHolder.textViewBedNumber.setText(Integer.toString(recordAndPatient.patient.getBedNumber()));
+                    }
+                });
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() { return examinationList.size(); }
+
+    public void setExaminationList(List<Object> examinationList) {
+        this.examinationList = examinationList;
+        notifyDataSetChanged();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Object object);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) { this.listener = listener; }
+}
